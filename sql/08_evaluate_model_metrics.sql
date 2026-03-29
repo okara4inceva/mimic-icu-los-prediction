bd08abf95fd631e0a1199369eebbea760d986e03
okara4inceva <93776649+okara4inceva@users.noreply.github.com>
2026-03-30 01:34:04 +0400

    08_evaluate_model_metrics.sql
    Model Evaluation (MAE, RMSE, R²)

--- sql/08_evaluate_model_metrics.sql
+++ sql/08_evaluate_model_metrics.sql
@@ -1,19 +1,27 @@
 -- ============================================
 -- Query: Model Evaluation (MAE, RMSE, R²)
--- Description: Evaluates model performance using multiple regression metrics.
--- Note: Project and dataset names are anonymized for sharing.
+-- Description: Evaluate regression model performance using standard metrics:
+-- Mean Absolute Error (MAE), Root Mean Squared Error (RMSE),
+-- and R-squared (R²).
+-- Source: MIMIC-IV (PhysioNet)
+-- Note: Dataset identifiers are anonymized for public sharing.
+-- Note: Model trained on log-transformed LOS to handle skewness
+-- MAE for interpretability in days, RMSE to capture larger errors, and R² to assess overall explanatory power of the model.
+-- Replace `project.dataset` with your own BigQuery environment.
 -- Author: Olga Karachyntseva
 -- ============================================
--- Example query (project name anonymized for sharing)
+
 SELECT
   mean_absolute_error AS mae,
   mean_squared_error AS mse,
   SQRT(mean_squared_error) AS rmse,
   r2_score
-FROM
-  ML.EVALUATE(
-    MODEL `your_project.your_dataset.model_name`,
-    (
-      SELECT * FROM `your_project.your_dataset.test_set`
-    )
-  );
+
+FROM ML.EVALUATE(
+  MODEL `project.dataset.los_model_v4_log`,
+  (
+    SELECT *
+    FROM `project.dataset.icu_ml_dataset_features_v3_fixed`
+    WHERE los_days BETWEEN 0 AND 30
+  )
+);
